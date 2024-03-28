d111c591b0637b9e7ed45e186167406932996f4f
teegalas <Teegala.Sai@amdocs.com>
2024-03-28 07:40:14 +0530

    Initial Spring Boot Skeleton

---
+++ src/main/java/com/bhargav/apigw/multiauthapp/MultiauthappApplication.java
@@ -0,0 +1,13 @@
+package com.bhargav.apigw.multiauthapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultiauthappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MultiauthappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bhargav/apigw/multiauthapp/MultiauthappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bhargav.apigw.multiauthapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MultiauthappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
